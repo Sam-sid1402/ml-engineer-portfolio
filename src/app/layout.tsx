fc61f6c2f5b9e40e29438f44343a4507bad33757
Semyon Sidorov <semyonsidorov@Semyons-MacBook-Air.local>
2026-06-13 19:17:47 +0200

    Add Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import Navbar from "@/components/layout/Navbar";
 import Footer from "@/components/layout/Footer";
 import { SITE } from "@/lib/constants";
 import "./globals.css";
-
+import { Analytics } from "@vercel/analytics/next"
 const inter = Inter({
   subsets: ["latin"],
   variable: "--font-inter",
